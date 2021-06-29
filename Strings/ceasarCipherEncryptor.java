21eb7d88b6ff4ce7b3ccf88dd6069caaa909d3a7
Ben Chavez <ben.m.chavez@gmail.com>
2021-06-28 21:19:40 -0500

    Ceasar Cipher Encryptor attempt 1. Challenge: I didn't think to use the modulo operator, which was the core of the challenge. I was able to solve, but got caught up on index out of bounds issues due to not using the modulo

---
+++ Strings/ceasarCipherEncryptor.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Program {
+  public static String caesarCypherEncryptor(String str, int key) {
+    // Time Complexity: O(n)  | Space Complexity: O(n)
+		
+		String alphabet = "0abcdefghijklmnopqrstuvwxyz";
+		StringBuilder shiftedStr = new StringBuilder();
+		System.out.println(str.length());
+		while (key > 26) {
+			key -= 26;
+		}
+		
+		for (int idx = 0; idx < str.length(); idx++){
+			int letterIdx = alphabet.indexOf(str.charAt(idx)) + key;
+			letterIdx = (letterIdx > 26) ? letterIdx - 26 : letterIdx;
+			shiftedStr.append(alphabet.charAt(letterIdx ));
+		}
+    return shiftedStr.toString();
+  }
+}
