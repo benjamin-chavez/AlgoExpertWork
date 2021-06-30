8a403bc73c76305f4d846818bc3f1e8aff201a26
Ben Chavez <ben.m.chavez@gmail.com>
2021-06-29 19:46:15 -0500

    ceasar cipher code: i think my solution is actually cleaner than the one that was provided. only after I learned that i should be using the modulo though..

--- Strings/ceasarCipherEncryptor.java
+++ Strings/ceasarCipherEncryptor.java
@@ -1,21 +1,40 @@
+// FINAL SOLUTION
 import java.util.*;
 
 class Program {
   public static String caesarCypherEncryptor(String str, int key) {
-    // Time Complexity: O(n)  | Space Complexity: O(n)
-		
+		char[] shiftedStrArray = new char[str.length()];
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		StringBuilder shiftedStr = new StringBuilder();
-		System.out.println(str.length());
-		while (key > 26) {
-			key -= 26;
-		}
 		
-		for (int idx = 0; idx < str.length(); idx++){
-			int letterIdx = alphabet.indexOf(str.charAt(idx)) + key;
-			letterIdx = (letterIdx >= 26) ? letterIdx - 26 : letterIdx;
-			shiftedStr.append(alphabet.charAt(letterIdx ));
+		for (int idx = 0; idx < str.length(); idx++) {
+			int preShiftedIdx = alphabet.indexOf(str.charAt(idx));
+			int postShiftedIdx = (preShiftedIdx + key) % 26;
+			shiftedStrArray[idx] = alphabet.charAt(postShiftedIdx);	
 		}
-    return shiftedStr.toString();
+			
+    return new String(shiftedStrArray);
   }
 }
+
+
+// import java.util.*;
+
+// class Program {
+//   public static String caesarCypherEncryptor(String str, int key) {
+//     // Time Complexity: O(n)  | Space Complexity: O(n)
+		
+// 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+// 		StringBuilder shiftedStr = new StringBuilder();
+// 		System.out.println(str.length());
+// 		while (key > 26) {
+// 			key -= 26;
+// 		}
+		
+// 		for (int idx = 0; idx < str.length(); idx++){
+// 			int letterIdx = alphabet.indexOf(str.charAt(idx)) + key;
+// 			letterIdx = (letterIdx >= 26) ? letterIdx - 26 : letterIdx;
+// 			shiftedStr.append(alphabet.charAt(letterIdx ));
+// 		}
+//     return shiftedStr.toString();
+//   }
+// }
