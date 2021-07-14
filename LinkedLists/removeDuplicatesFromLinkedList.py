e93017c6a6f7b773c64e55d1e02e1108a6b454d4
Ben Chavez <ben.m.chavez@gmail.com>
2021-07-13 20:37:04 -0500

    remove duplicates from linked list -python solution

---
+++ LinkedLists/removeDuplicatesFromLinkedList.py
@@ -0,0 +1,21 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# O(n) time | O(1) space - where n is the number of nodes in the Linked List
+def removeDuplicatesFromLinkedList(linkedList):
+	# Create two pointers: curNode, nextNode
+	curNode = linkedList
+	nextNode = linkedList.next
+	# Iterate through the linked list:
+	while nextNode is not None:
+		#	if the value of the curNode is equal to the value of the nextNode, chang curNode.next to the subsequent Node
+		if (curNode.value == nextNode.value):
+			curNode.next = nextNode.next
+		else:
+			curNode = curNode.next
+		nextNode = nextNode.next
+	# Return the modified LinkedList
+	return linkedList
